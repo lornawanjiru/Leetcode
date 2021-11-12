697df59c3474ab103317df93fa6144097e09f509
lornawanjiru <lornamuchangi@gmail.com>
2021-11-12 02:44:21 -0800

    twosums

--- Twosums.js
+++ Twosums.js
@@ -3,7 +3,19 @@
 //   @param {number} target
 //   @return {number[]}
  
- var twoSum = function(nums, target) {
-    var nums = [];
-    var target 
- };
+ // Lets take an example of :-
+//    array = [0,3,5,7]
+//    target = 10
+// [0] + [1] = 3 No 
+// [0] + [2]  = 5 No 
+// i = first index
+// j = i + 1
+var twoSum = function(nums, target) {
+   for (i=0; i<nums.length; i++){
+       for(j=i+1; j<nums.length; j++){
+           if(nums[i] + nums[j] === target){
+               return [i,j];
+           }
+       }
+   }
+};
