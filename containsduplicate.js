d6de777464f87619ec54da9ffc34b162ddb91b97
lornawanjiru <lornamuchangi@gmail.com>
2021-11-24 00:04:56 -0800

    Containsduplicate1

--- containsduplicate.js
+++ containsduplicate.js
@@ -1,2 +1,18 @@
-let set = new Set([1,2,3,3,5,4,2,1]);
-console.log(set.size);
+// We all know Set object only holds unique values. 
+// So if we add all the elements from the array into the set,
+// the length of set and array should be equal if there are no duplicates.
+
+
+var containsDuplicate = function(nums) {
+    // adding array elements into the Set
+    let arr = new Set (nums);
+    // Comparing the array length to the Set size 
+     if (arr.size === nums.length){
+        //  If they are equal 
+         return false;
+     }
+     else {
+        //  if not
+         return true;
+     }
+ };
