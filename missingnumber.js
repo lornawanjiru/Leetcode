8cb443dce4ff30974e7689ae49e9cbafc20601cb
Lorna Wanjiru <49642852+lornawanjiru@users.noreply.github.com>
2021-12-23 04:38:36 -0800

    Update missingnumber.js

--- missingnumber.js
+++ missingnumber.js
@@ -12,3 +12,18 @@ var missingNumber = function(nums) {
 // It did make my time complexity less thats the only difference    
 return -1;
 };
+
+//Second option
+
+var missingNumber = function(nums) {
+    //using the gauss formular to sum up numbers in a sequence: 
+    const gsum = (nums.length*(nums.length+1))/2;
+    
+    //Using the reduce method to sum up numbers in the current array
+    const nsum = nums.reduce((acc,cur)=> acc+cur ,0);
+    
+    //Since the gsum is less than the current array sum then the value missing is the result of the subtraction
+    const mnum = gsum - nsum;
+    
+    return mnum;
+};
